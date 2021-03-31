8e97075f197ab2620b6f5d91adf5c4ee3b8dc60e
Dieter <dieter.vanderelst@fastmail.net>
2021-03-31 12:46:28 -0400

    some editing

--- preprocess_between.py
+++ preprocess_between.py
@@ -2,116 +2,118 @@ import pandas
 import library
 import numpy
 
-data = pandas.read_csv('Ethics+in+patient+care_March+27,+2021_15.51.csv')
-data = data.query('Finished=="True"')
-data = data.query('Status == "IP Address"')  # remove previews
-line1 = data.iloc[0, :]
-line2 = data.iloc[1, :]
-data = data.iloc[2:, :]
 
-# Who saw what?
-x = data.loc[:, ('ResponseId', 'actor_condition')]
-print(x)
+def run():
+    data = pandas.read_csv('Ethics+in+patient+care_March+27,+2021_15.51.csv')
+    data = data.query('Finished=="True"')
+    data = data.query('Status == "IP Address"')  # remove previews
+    line1 = data.iloc[0, :]
+    line2 = data.iloc[1, :]
+    data = data.iloc[2:, :]
 
-# Merge different options for ethnicity into a single variable
-subset, selected_variables = library.select_variables(data, ['Q9_'])
-data['ethnicity'] = library.concatenate_columns(subset)
-for x in range(10): data['ethnicity'] = data['ethnicity'].str.replace('_nan', '')
-for x in range(10): data['ethnicity'] = data['ethnicity'].str.replace('nan_', '')
+    # Who saw what?
+    x = data.loc[:, ('ResponseId', 'actor_condition')]
+    print(x)
 
-# Get rating data
-rating_variables = ['ResponseId', '_Q18_', '_Q19_', '_Q29_', '_Q30_']
-subset, selected_variables = library.select_variables(data, rating_variables)
-names = ['scenario', 'question', 'scale']
-rating_data = library.split_qualtrics_variables(subset, 'ResponseId', column_names=names)
-rating_data = rating_data.dropna()
-rating_data = rating_data.iloc[:, [0, 2, 3, 4, 5]]
+    # Merge different options for ethnicity into a single variable
+    subset, selected_variables = library.select_variables(data, ['Q9_'])
+    data['ethnicity'] = library.concatenate_columns(subset)
+    for x in range(10): data['ethnicity'] = data['ethnicity'].str.replace('_nan', '')
+    for x in range(10): data['ethnicity'] = data['ethnicity'].str.replace('nan_', '')
 
-rating_data.rename(columns={'value': 'rating'}, inplace=True)
-rating_data['scenario'] = rating_data['scenario'].astype(int)
-rating_data['rating'] = rating_data['rating'].astype(int)
-rating_data['scale'] = rating_data['scale'].astype(int)
-rating_data = rating_data.replace('Q29', 'Q18')
-rating_data = rating_data.replace('Q30', 'Q19')
+    # Get rating data
+    rating_variables = ['ResponseId', '_Q18_', '_Q19_', '_Q29_', '_Q30_']
+    subset, selected_variables = library.select_variables(data, rating_variables)
+    names = ['scenario', 'question', 'scale']
+    rating_data = library.split_qualtrics_variables(subset, 'ResponseId', column_names=names)
+    rating_data = rating_data.dropna()
+    rating_data = rating_data.iloc[:, [0, 2, 3, 4, 5]]
 
-# Get scenarios
-scenarios = pandas.read_csv('scenarios.csv', sep=',')
-scenarios['scenario'] = numpy.arange(36) + 1
+    rating_data.rename(columns={'value': 'rating'}, inplace=True)
+    rating_data['scenario'] = rating_data['scenario'].astype(int)
+    rating_data['rating'] = rating_data['rating'].astype(int)
+    rating_data['scale'] = rating_data['scale'].astype(int)
+    rating_data = rating_data.replace('Q29', 'Q18')
+    rating_data = rating_data.replace('Q30', 'Q19')
 
-# Merge ratings and scenarios
-rating_data = pandas.merge(rating_data, scenarios, on='scenario')
+    # Get scenarios
+    scenarios = pandas.read_csv('scenarios.csv', sep=',')
+    scenarios['scenario'] = numpy.arange(36) + 1
 
-# Double check: each participant should only have one kind of actor
-grp = rating_data.groupby(['ResponseId', 'actor'])
-cnts = grp.count()
-print(cnts['rating'])
+    # Merge ratings and scenarios
+    rating_data = pandas.merge(rating_data, scenarios, on='scenario')
 
-# %% Get validation data
-validation_variables = ['ResponseId', '_Q22', '_Q23', '_Q33_', '_Q34']
-subset, selected_variables = library.select_variables(data, validation_variables)
-names = ['scenario', 'question']
-validation_data = library.split_qualtrics_variables(subset, 'ResponseId', column_names=names)
-validation_data.rename(columns={'value': 'selection'}, inplace=True)
-validation_data = validation_data.dropna()
-validation_data = validation_data.iloc[:, [0, 2, 3, 4]]
+    # Double check: each participant should only have one kind of actor
+    grp = rating_data.groupby(['ResponseId', 'actor'])
+    cnts = grp.count()
+    print(cnts['rating'])
 
-validation_data['scenario'] = validation_data['scenario'].astype(int)
-validation_data.selection = validation_data.selection.str.lower()
-validation_data = validation_data.replace('Q33', 'Q22')
-validation_data = validation_data.replace('Q34', 'Q23')
+    # %% Get validation data
+    validation_variables = ['ResponseId', '_Q22', '_Q23', '_Q33_', '_Q34']
+    subset, selected_variables = library.select_variables(data, validation_variables)
+    names = ['scenario', 'question']
+    validation_data = library.split_qualtrics_variables(subset, 'ResponseId', column_names=names)
+    validation_data.rename(columns={'value': 'selection'}, inplace=True)
+    validation_data = validation_data.dropna()
+    validation_data = validation_data.iloc[:, [0, 2, 3, 4]]
 
-# Merge validation data and scenarios
-validation_data = pandas.merge(validation_data, scenarios, on='scenario')
-validation_data['relevant'] = False
+    validation_data['scenario'] = validation_data['scenario'].astype(int)
+    validation_data.selection = validation_data.selection.str.lower()
+    validation_data = validation_data.replace('Q33', 'Q22')
+    validation_data = validation_data.replace('Q34', 'Q23')
 
-selection = validation_data.question == 'Q22'
-validation_data.relevant[selection] = validation_data.condition[selection]
+    # Merge validation data and scenarios
+    validation_data = pandas.merge(validation_data, scenarios, on='scenario')
+    validation_data['relevant'] = False
 
-selection = validation_data.question == 'Q23'
-validation_data.relevant[selection] = validation_data.action[selection]
+    selection = validation_data.question == 'Q22'
+    validation_data.relevant[selection] = validation_data.condition[selection]
 
-validation_data['correct'] = validation_data.selection.str[0] == validation_data.relevant.str[0]
-validation_data = validation_data.iloc[:, [0, 1, 2, 3, 11]]
+    selection = validation_data.question == 'Q23'
+    validation_data.relevant[selection] = validation_data.action[selection]
 
-grp = validation_data.groupby(['ResponseId', 'scenario'])
-mns = grp.mean()
-mns = mns.reset_index()
+    validation_data['correct'] = validation_data.selection.str[0] == validation_data.relevant.str[0]
+    validation_data = validation_data.iloc[:, [0, 1, 2, 3, 11]]
 
-# Add validation data to ratings
-rating_data = rating_data.merge(mns, on=['ResponseId', 'scenario'])
+    grp = validation_data.groupby(['ResponseId', 'scenario'])
+    mns = grp.mean()
+    mns = mns.reset_index()
 
-# Renumber scales
-grp = rating_data.groupby(['question', 'scale'])
-cnt = grp.count()
-cnt = cnt.reset_index()
-cnt = cnt.iloc[:, [0, 1]]
-cnt['renumbered_scale'] = ['s1', 's2', 's3', 's4', 's5', 's6']
-rating_data = pandas.merge(rating_data, cnt, on=['question', 'scale'])
+    # Add validation data to ratings
+    rating_data = rating_data.merge(mns, on=['ResponseId', 'scenario'])
 
-# %% Get demographic data
-demographics_variables = ['ResponseId', 'Q5', 'Q6', 'Q7', 'Q8', 'ethnicity']
-subset, selected_variables = library.select_variables(data, demographics_variables)
-names = ['question', 'option', 'dummy']
-demo_data = library.split_qualtrics_variables(subset, 'ResponseId', column_names=names)
-demo_data = demo_data.loc[:, ('ResponseId', 'question', 'value')]
-demo_data = demo_data.dropna()
+    # Renumber scales
+    grp = rating_data.groupby(['question', 'scale'])
+    cnt = grp.count()
+    cnt = cnt.reset_index()
+    cnt = cnt.iloc[:, [0, 1]]
+    cnt['renumbered_scale'] = ['s1', 's2', 's3', 's4', 's5', 's6']
+    rating_data = pandas.merge(rating_data, cnt, on=['question', 'scale'])
 
-# %%
-with pandas.ExcelWriter('data/preprocessed_between.xls') as writer:
-    rating_data.to_excel(writer, sheet_name='rating_data')
-    demo_data.to_excel(writer, sheet_name='demo_data')
-    validation_data.to_excel(writer, sheet_name='validation_data')
-    scenarios.to_excel(writer, sheet_name='scenarios')
+    # %% Get demographic data
+    demographics_variables = ['ResponseId', 'Q5', 'Q6', 'Q7', 'Q8', 'ethnicity']
+    subset, selected_variables = library.select_variables(data, demographics_variables)
+    names = ['question', 'option', 'dummy']
+    demo_data = library.split_qualtrics_variables(subset, 'ResponseId', column_names=names)
+    demo_data = demo_data.loc[:, ('ResponseId', 'question', 'value')]
+    demo_data = demo_data.dropna()
 
-# %%
-print('before:', rating_data.shape)
-rating_data_subset = rating_data.query('correct == 1')
-print('after:', rating_data_subset.shape)
+    # %%
+    with pandas.ExcelWriter('data/preprocessed_between.xls') as writer:
+        rating_data.to_excel(writer, sheet_name='rating_data')
+        demo_data.to_excel(writer, sheet_name='demo_data')
+        validation_data.to_excel(writer, sheet_name='validation_data')
+        scenarios.to_excel(writer, sheet_name='scenarios')
 
-data_table = rating_data_subset.pivot(index=('ResponseId', 'scenario'), columns='renumbered_scale', values='rating')
-data_table = data_table.reset_index()
-data_table['summed'] = data_table.s4 + data_table.s5 + data_table.s6
-data_table = pandas.merge(data_table, scenarios, on='scenario')
+    # %%
+    print('before:', rating_data.shape)
+    rating_data_subset = rating_data.query('correct == 1')
+    print('after:', rating_data_subset.shape)
 
-with pandas.ExcelWriter('data/data_table_between.xls') as writer:
-    data_table.to_excel(writer, sheet_name='data_table')
+    data_table = rating_data_subset.pivot(index=('ResponseId', 'scenario'), columns='renumbered_scale', values='rating')
+    data_table = data_table.reset_index()
+    data_table['summed'] = data_table.s4 + data_table.s5 + data_table.s6
+    data_table = pandas.merge(data_table, scenarios, on='scenario')
+
+    with pandas.ExcelWriter('data/data_table_between.xls') as writer:
+        data_table.to_excel(writer, sheet_name='data_table')
--- process.py
+++ process.py
@@ -1,28 +1,52 @@
 import pandas
 import seaborn
 from matplotlib import pyplot
-import numpy
-from pyBat import Statistics
+import preprocess_within
+import preprocess_between
 
-# rating_data = pandas.read_excel('data/preprocessed_between.xls', sheet_name='rating_data', index_col=0)
-#
-#
-# seaborn.catplot(x='renumbered_scale', y='rating', hue='actor', data=rating_data, col='condition_rank', kind='point')
-# pyplot.show()
+preprocess_within.run()
+preprocess_between.run()
 
 for x in range(2):
     if x == 0: data_table = pandas.read_excel('data/data_table_within.xls', sheet_name='data_table', index_col=0)
     if x == 1: data_table = pandas.read_excel('data/data_table_between.xls', sheet_name='data_table', index_col=0)
     data_table['s7'] = ((data_table.s4 + data_table.s5 + data_table.s6) / 3)
+    data_table['impact'] = ((data_table.s1 + data_table.s2 + data_table.s3) / 3)
     if x == 0:
-        data_table['experiment'] = 'within'
+        data_table['Design'] = 'within'
         within = data_table
     if x == 1:
-        data_table['experiment'] = 'between'
+        data_table['Design'] = 'between'
         between = data_table
 
 all_data = pandas.concat((between, within))
+all_data['Action'] = all_data['action_rank']
+all_data['Rating'] = all_data['s7']
+all_data['Malady'] = all_data['condition_rank']
+#%% Get impact ratings
+impact_ratings = all_data.loc[:, ('ResponseId','Design','Action','s1', 's2', 's3')]
+impact_ratings = pandas.melt(impact_ratings, id_vars=('ResponseId','Design','Action'))
+seaborn.catplot(x='Action', y='value', hue='variable', kind='point', col='Design', data=impact_ratings)
+pyplot.show()
 
-formula = 's7 ~ s1 + s2 + s3 + C(actor)'
-result = Statistics.regression(formula, between)
-print(result['summary'])
+impact_ratings = all_data.loc[:, ('ResponseId','Design','Action','impact')]
+impact_ratings = pandas.melt(impact_ratings, id_vars=('ResponseId','Design','Action'))
+seaborn.catplot(x='Action', y='value', hue='variable', kind='point', col='Design', data=impact_ratings)
+pyplot.show()
+
+#%%
+facet_kws=dict(despine=False)
+
+seaborn.set_style('darkgrid')
+seaborn.catplot(x='Action', y='Rating', hue='actor', kind='point', col='Malady', row='Design',facet_kws=facet_kws, data=all_data)
+pyplot.ylim(0, 100)
+pyplot.tight_layout()
+pyplot.show()
+
+subset = all_data.query('action_rank in [0,5]')
+subset = subset.query('condition_rank in [0,2]')
+seaborn.set_style('darkgrid')
+seaborn.catplot(x='Malady', y='Rating', hue='actor', kind='point', col='Action', row='Design',facet_kws=facet_kws, data=subset)
+pyplot.ylim(0, 100)
+pyplot.tight_layout()
+pyplot.show()
